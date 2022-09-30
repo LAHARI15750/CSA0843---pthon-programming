5815e13d1485e31f8baa4859f104a7737a6a61df
LAHARI15750 <112243697+LAHARI15750@users.noreply.github.com>
2022-09-30 09:40:20 +0530

    Add files via upload

---
+++ alphabetical order.py
@@ -0,0 +1,22 @@
+n=int(input("Enter the number of elements="))
+arr=[]
+for i in range(0,n):
+    a=input("Enter the string=")
+    arr.append(a)
+print(arr)
+ch=input("Enter the order required ASCENDING-A or DESCENDING-D ")
+if(ch=='A' or ch=='a'):
+    for i in range(0,n):
+        for j in range(i+1,n):
+            if(arr[i]>arr[j]):
+                temp=arr[i]
+                arr[i]=arr[j]
+                arr[j]=temp
+    print("THE ASCENDING ORDER")
+    for i in range(0,n):
+        print(arr[i])
+else:
+        print("THE DESCENDING ORDER")
+        for i in range(0,n):
+            print(arr[i])
+        
