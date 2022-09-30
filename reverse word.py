bfaa257e60bc389f9d471b1317f21f18183ef82e
LAHARI15750 <112243697+LAHARI15750@users.noreply.github.com>
2022-09-30 10:37:11 +0530

    Add files via upload

---
+++ reverse word.py
@@ -0,0 +1,8 @@
+def reverse(str):
+    string="".join(reversed(str))
+    for i in s:
+     return string
+s="SIGN UP"
+print("the original string is: ",s)
+print("the reversed string using reversed() is:",reverse(s))
+
